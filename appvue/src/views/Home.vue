9d192c04532fd0fb5e3a58cc39332269cd5ea2fa
yandry <yandry75@gmail.com>
2021-11-17 03:04:25 +0100

    Skill and About me sections

--- appvue/src/views/Home.vue
+++ appvue/src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <!--Background image and title-->
   <div class="">
-    <main class="px-2 sm:px-6 md:mt-16 lg:mt-20 lg:px-12 xl:mt-28">
-      <div class="sm:text-center lg:text-center">
+    <main class="px-3 sm:px-6 md:mt-16 lg:mt-20 lg:px-12 xl:mt-28">
+      <div class="px-5 sm:text-center lg:text-center">
         <!--title-->
         <h1
           class="
@@ -15,9 +15,9 @@
           "
         >
           <span class="block xl:inline">Yandry Villagómez</span>
-          <div>
+          <div >
             <span
-              class="
+              class="profesion
                 block
                 bg-clip-text
                 text-transparent
@@ -29,7 +29,7 @@
               >Fullstack</span
             >
             <span
-              class="
+              class="profesion
                 block
                 bg-clip-text
                 text-transparent
@@ -53,17 +53,19 @@
             lg:text-center
             md:text-2xl
             sm:text-lg
+            
+            font-bold
           "
         >
           Creando aplicaciones bonitas y útiles
         </p>
 
         <div class="flex flex-wrap justify-center mx-auto mt-8">
-          <div class="w-6/12 sm:w-3/12 px-4">
+          <div class=" w-6/12 sm:w-3/12 px-4">
             <img
               src="../assets/imagenes/portada1.jpeg"
               alt="..."
-              class="
+              class="perfil
                 shadow
                 rounded-full
                 max-w-full
@@ -78,7 +80,7 @@
         <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-center">
           <div class="rounded-md border-opacity-100">
             <a
-              href="#"
+              href=""
               class="
                 w-full
                 flex
@@ -126,25 +128,103 @@
       </div>
     </main>
 
-    <div class=" py-6 md:py-28 mt-3">
+    <div class=" py-6 md:py-28 mt-3 md:px-24">
+      <!--About me-->
       <div class="text-3xl md:text-5xl font-extrabold mx-12">
-        <span
+        <div class="flex flex-row justify-items-center items-center ">
+          <span
           class="
+            
             bg-clip-text
             text-transparent
             bg-gradient-to-r
             from-green-400
             to-blue-500
           "
-          >About me
-        </span>
-        <p class="text-white text-base md:text-xl mt-4">
-          A muy temprana edad me empezo a llamar la atencion la programacion . Con 15 años hacia scripts en Windows Xp y desmontaba ordenadores como pasatiempo 
-          .Asi es como descubri que me gustaba programar y me especialize como desarrollador de aplicaciones web . Programar para mi es divertirme y a la vez poder crear cosas asombrosas  
+          >A cerca de mi 
+        </span><img  class="mt-2 mx-3 w-8 sm:w-11" src="../assets/icons/icons8-metal-music-48.png">
+        </div>
+       
+        <p class="about text-white text-base  mt-4 md:text-xl text-justify leading-normal"  >
+          A muy temprana edad me empezó a llamar la atención programar . A los 15 años hacía scripts en Windows Xp y desmontaba ordenadores como pasatiempo 
+          .Así descubrí que me gustaba programar y me especialicé como desarrollador de aplicaciones web . Programar para mi es divertirme y a la vez poder crear cosas asombrosas.  
         </p>
       </div>
+
+      <!--Skills-->
+      <div class="flex justify-between flex-wrap items-center">
+        <div class=" text-3xl md:text-5xl font-extrabold mx-12 mt-3">
+          <span
+            class="
+              bg-clip-text
+              text-transparent
+              bg-gradient-to-r
+              from-green-400
+              to-blue-500
+            "
+            >Habilidades
+          </span>
+          <ul class="list-disc list-inside m-12">
+            <li class="text-white">Autodidacta</li>
+            <li class="text-white">Creativo</li>
+            <li class="text-white">Responsable</li>
+            <li class="text-white">Productivo</li>
+          </ul>
+        </div>
+        <div class="mx-32">
+          <img src="../assets/icons/Creativity(1).gif" alt="" > 
+        </div>
+      </div>
+    
+      
+
+
     </div>
+
+    
   </div>
 </template>
 
-<script></script>
+<script>
+
+
+</script>
+
+<style scoped>
+
+
+.profesion{
+  animation-duration: 3s;
+  animation-name: slidein;
+  animation-iteration-count: 20s;
+  animation-direction:normal
+}
+
+
+@keyframes slidein {
+  from {
+    margin-left: 100%;
+    width: 300%
+  }
+
+  to {
+    margin-left: 0%;
+    width: 100%;
+  }
+}
+
+
+
+	
+.perfil:hover {
+
+ box-shadow: 0px 0px 15px 15px #7faedf;
+ -webkit-transform: rotateY(180deg);
+-webkit-transform-style: preserve-3d;
+transform: rotateY(180deg);
+transform-style: preserve-3d;
+}
+
+
+
+</style>
