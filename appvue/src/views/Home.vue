f0147c952bbb89ad4484aacd056e4a8b17c78e3d
yandry <yandry75@gmail.com>
2021-11-20 18:59:16 +0100

    ok

--- appvue/src/views/Home.vue
+++ appvue/src/views/Home.vue
@@ -1,133 +1,132 @@
 <template>
   <!--Background image and title-->
-  
-    <main class="px-3 sm:px-6 md:mt-16 lg:mt-20 lg:px-12 xl:mt-28">
-      <div class="px-5 sm:text-center lg:text-center">
-        <!--title-->
-        <h1
-          class="
-            text-4xl
-            tracking-tight
-            font-extrabold
-            text-white
-            sm:text-5xl
-            md:text-6xl
-          "
-        >
-          <span class="block xl:inline">Yandry Villagómez</span>
-          <div>
-            <span
-              class="
-                profesion
-                block
-                bg-clip-text
-                text-transparent
-                bg-gradient-to-r
-                from-green-400
-                to-blue-50
-                xl:inline
-              "
-              >Fullstack</span
-            >
-            <span
-              class="
-                profesion
-                block
-                bg-clip-text
-                text-transparent
-                bg-gradient-to-r
-                from-red-400
-                to-blue-500
-                xl:inline
-              "
-              >Developer</span
-            >
-          </div>
-        </h1>
 
-        <p
-          class="
-            mt-5
-            text-white text-base
-            tracking-tight
-            sm:text-center
-            lg:text-center
-            md:text-2xl
-            sm:text-lg
-            font-bold
-          "
-        >
-          Creando aplicaciones bonitas y útiles
-        </p>
-
-        <div class="flex flex-wrap justify-center mx-auto mt-8">
-          <div class="w-6/12 sm:w-3/12 px-4">
-            <img
-              src="../assets/imagenes/portada1.jpeg"
-              alt="..."
-              class="
-                perfil
-                shadow
-                rounded-full
-                max-w-full
-                h-auto
-                align-middle
-                border-none
-              "
-            />
-          </div>
+  <main class="px-3 sm:px-6 md:mt-16 lg:mt-20 lg:px-12 xl:mt-28">
+    <div class="px-5 sm:text-center lg:text-center">
+      <!--title-->
+      <h1
+        class="
+          text-4xl
+          tracking-tight
+          font-extrabold
+          text-white
+          sm:text-5xl
+          md:text-6xl
+        "
+      >
+        <span class="block xl:inline">Yandry Villagómez</span>
+        <div>
+          <span
+            class="
+              profesion
+              block
+              bg-clip-text
+              text-transparent
+              bg-gradient-to-r
+              from-green-400
+              to-blue-50
+              xl:inline
+            "
+            >Fullstack</span
+          >
+          <span
+            class="
+              profesion
+              block
+              bg-clip-text
+              text-transparent
+              bg-gradient-to-r
+              from-red-400
+              to-blue-500
+              xl:inline
+            "
+            >Developer</span
+          >
         </div>
-        <!--Buttons-->
-        <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-center">
-          <div class="rounded-md border-opacity-100">
-            <a
-              href=""
-              class="
-                w-full
-                flex
-                items-center
-                justify-center
-                px-8
-                py-3
-                border border-transparent
-                text-base
-                font-medium
-                rounded-md
-                text-white
-                bg-indigo-600
-                hover:bg-indigo-700
-                md:py-4 md:text-lg md:px-10
-              "
-            >
-              Acerca de mi
-            </a>
-          </div>
-          <div class="mt-3 sm:mt-0 sm:ml-3">
-            <a
-              href="#"
-              class="
-                w-full
-                flex
-                items-center
-                justify-center
-                px-8
-                py-3
-                border border-transparent
-                text-base
-                font-medium
-                rounded-md
-                text-indigo-700
-                bg-indigo-100
-                hover:bg-indigo-200
-                md:py-4 md:text-lg md:px-10
-              "
-            >
-              Contacto
-            </a>
-          </div>
+      </h1>
+
+      <p
+        class="
+          mt-5
+          text-white text-base
+          tracking-tight
+          sm:text-center
+          lg:text-center
+          md:text-2xl
+          sm:text-lg
+          font-bold
+        "
+      >
+        Creando aplicaciones bonitas y útiles
+      </p>
+
+      <div class="flex flex-wrap justify-center mx-auto mt-8">
+        <div class="w-6/12 sm:w-3/12 px-4">
+          <img
+            src="../assets/imagenes/portada1.jpeg"
+            alt="..."
+            class="
+              perfil
+              shadow
+              rounded-full
+              max-w-full
+              h-auto
+              align-middle
+              border-none
+            "
+          />
         </div>
       </div>
-    </main>
+      <!--Buttons-->
+      <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-center">
+        <div class="rounded-md border-opacity-100">
+          <a
+            href=""
+            class="
+              w-full
+              flex
+              items-center
+              justify-center
+              px-8
+              py-3
+              border border-transparent
+              text-base
+              font-medium
+              rounded-md
+              text-white
+              bg-indigo-600
+              hover:bg-indigo-700
+              md:py-4 md:text-lg md:px-10
+            "
+          >
+            Acerca de mi
+          </a>
+        </div>
+        <div class="mt-3 sm:mt-0 sm:ml-3">
+          <a
+            href="#"
+            class="
+              w-full
+              flex
+              items-center
+              justify-center
+              px-8
+              py-3
+              border border-transparent
+              text-base
+              font-medium
+              rounded-md
+              text-indigo-700
+              bg-indigo-100
+              hover:bg-indigo-200
+              md:py-4 md:text-lg md:px-10
+            "
+          >
+            Contacto
+          </a>
+        </div>
+      </div>
+    </div>
 
     <div class="md:py-28 mt-3 md:px-24">
       <!--About me-->
@@ -208,49 +207,41 @@
         </span>
       </div>
 
-       
-     <div class="flex flex-wrap">
-      
+      <div class="flex flex-row">
         <img
-        class="mt-3 w-2/12 sm:w-2/12"
-        src="../assets/icons/html.svg"
-        alt="Sunset in the mountains"
-        />
-       
-     
-        <img
-        class="mt-3 w-2/12 sm:w-2/12"
-        src="../assets/icons/mysql.png"
-        alt="Sunset in the mountains"
-        />
-        <img
-        class="mt-3 w-2/12 sm:w-2/12"
-        src="../assets/icons/css.svg"
-        alt="Sunset in the mountains"
-        />
-        <img
-        class="mt-3 w-2/12 sm:w-2/12"
-        src="../assets/icons/java.png"
-        alt="Sunset in the mountains"
-        />
-        <img
-        class="mt-3 w-2/12 sm:w-2/12"
-        src="../assets/icons/python.png"
-        alt="Sunset in the mountains"
-        />
-        <img
-        class="mt-3 w-2/12 sm:w-2/12"
-        src="../assets/icons/javascript.png"
-        alt="Sunset in the mountains"
+          class="flex flex-wrap w-2/12 sm:w-2/12"
+          src="../assets/icons/html.svg"
+          alt="Sunset in the mountains"
         />
 
-
-     </div>
-      
-       
-
-
-  </div>
+        <img
+          class="mt-3 w-2/12 sm:w-2/12"
+          src="../assets/icons/mysql.png"
+          alt="Sunset in the mountains"
+        />
+        <img
+          class="mt-3 w-2/12 sm:w-2/12"
+          src="../assets/icons/css.svg"
+          alt="Sunset in the mountains"
+        />
+        <img
+          class="mt-3 w-2/12 sm:w-2/12"
+          src="../assets/icons/java.png"
+          alt="Sunset in the mountains"
+        />
+        <img
+          class="mt-3 w-2/12 sm:w-2/12"
+          src="../assets/icons/python.png"
+          alt="Sunset in the mountains"
+        />
+        <img
+          class="mt-3 w-2/12 sm:w-2/12"
+          src="../assets/icons/javascript.png"
+          alt="Sunset in the mountains"
+        />
+      </div>
+    </div>
+  </main>
 </template>
 
 <script></script>
